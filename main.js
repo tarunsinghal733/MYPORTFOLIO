a06dc581889122cf42663a1a0174c394d5e3cf6b
TARUN SINGHAL <tarunsinghal733>
2021-07-18 23:17:34 +0530

    TARUJN

---
+++ main.js
@@ -0,0 +1,13 @@
+/*sticky navbar*/
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section-1").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
